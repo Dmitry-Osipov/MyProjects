9fc78b7b82b7d46604f7fe962ac03424e646396f
Dmitry-Osipov <dimaosipov00@gmail.com>
2023-07-30 18:13:40 +0300

    created lesson4 and update lesson1, lesson2, lesson3

--- Python_Homework/lesson3.py
+++ Python_Homework/lesson3.py
@@ -1,3 +1,5 @@
+from random import randint
+
 # Дана последовательность из N целых чисел и число K. Необходимо сдвинуть всю последовательность
 # (сдвиг - циклический) на K элементов вправо,  K – положительное число.
 # Input:   [1, 2, 3, 4, 5] k = 3
@@ -11,7 +13,6 @@ for i in range(k):
     lst.insert(0, num)
 
 print(lst)
-
 
 # Напишите программу для печати всех уникальных значений в словаре.
 # Input:  [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"}, {"VI": "S005"}, {"VII": "S005"}, {"V":"S009"}, {"VIII":"S007"}]
@@ -28,7 +29,6 @@ for dict_list in all_dict:
         my_set.add(item)
 
 print(my_set)
-
 
 # Дан массив, состоящий из целых чисел. Напишите программу, которая подсчитает количество элементов массива,
 # больших предыдущего (элемента с предыдущим номером)
@@ -53,7 +53,6 @@ else:
 
 print(count)
 
-
 # Задача 16: Требуется вычислить, сколько раз встречается некоторое число X в массиве A[N].
 # Пользователь в первой строке вводит натуральное число N – количество элементов в массиве.
 # В последующих  строках записаны N целых чисел Ai. Последняя строка содержит число X
@@ -62,7 +61,17 @@ print(count)
 # 3
 # -> 1
 
+new_list = [1, 2, 3, 4, 5, 5, 1, 2, 4, 1, 3, 2, 1, 1, 6, 8]
+user_number = int(input('Input number: '))
+count = 0
 
+if user_number in new_list:
+    for item in new_list:
+        if user_number == item:
+            count += 1
+    print(count)
+else:
+    print('Your number is not in the list')
 
 # Задача 18: Требуется найти в массиве A[N] самый близкий по величине элемент к заданному числу X.
 # Пользователь в первой строке вводит натуральное число N – количество элементов в массиве.
@@ -73,7 +82,25 @@ print(count)
 #
 # -> 5
 
+user_size = int(input('Input list size: '))
+user_number = int(input('Input number: '))
+user_list = []
 
+for i in range(user_size):
+    number = randint(-10000, 10000)
+    user_list.append(number)
+
+closest_number = user_list[0]
+min_distance = abs(user_list[0] - user_number)
+
+for number in user_list:
+    distance = abs(number - user_number)
+    if distance < min_distance:
+        min_distance = distance
+        closest_number = number
+
+print(f"Your list - {sorted(user_list)}\n" # Для удобства вывода
+      f"Nearest elem to {user_number} is - {closest_number}")
 
 # Задача 20: В настольной игре Скрабл (Scrabble) каждая буква имеет определенную ценность.
 # В случае с английским алфавитом очки распределяются так:
@@ -99,3 +126,38 @@ print(count)
 # ноутбук
 # Вывод:
 # 12
+
+one_point = ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'S', 'T', 'R', 'А', 'В', 'Е', 'И', 'Н', 'О', 'Р', 'С', 'Т']
+two_point = ['D', 'G', 'Д', 'К', 'Л', 'М', 'П', 'У']
+three_point = ['B', 'C', 'M', 'P', 'Б', 'Г', 'Ё', 'Ь', 'Я']
+four_point = ['F', 'H', 'V', 'W', 'Y', 'Й', 'Ы']
+five_point = ['K', 'Ж', 'З', 'Х', 'Ц', 'Ч']
+eight_point = ['J', 'X', 'Ш', 'Э', 'Ю']
+ten_point = ['Q', 'Z', 'Ф', 'Щ', 'Ъ']
+
+point_dict = {}
+for key in one_point:
+    point_dict[key] = 1
+for key in two_point:
+    point_dict[key] = 2
+for key in three_point:
+    point_dict[key] = 3
+for key in four_point:
+    point_dict[key] = 4
+for key in five_point:
+    point_dict[key] = 5
+for key in eight_point:
+    point_dict[key] = 8
+for key in ten_point:
+    point_dict[key] = 10
+
+count = 0
+user_word = input('Input one word without symbols: ').upper()
+
+try:
+    for letter in user_word:
+        count += point_dict[letter]
+
+    print(count)
+except KeyError:
+    print('Please input only one word without symbols')
---
+++ Python_Homework/lesson4.py
@@ -0,0 +1,92 @@
+from random import randint
+
+# Напишите программу, которая принимает на вход строку, и отслеживает, сколько раз каждый символ уже встречался.
+# Количество повторов добавляется к символам с помощью постфикса формата _n.
+# Input: a a a b c a a d c d d
+# Output: a a_1 a_2 b c a_3 a_4 d c_1 d_1 d_2
+# Для решения данной задачи используйте функцию .split()
+
+letter_list = input('Введите буквы от a до d: ').split()
+
+new_dict = {}
+new_string = ''
+
+for item in letter_list:
+    if item not in new_dict:
+        new_dict[item] = 0
+        new_string += f'{item} '
+    else:
+        new_dict[item] += 1
+        new_string += f'{item}_{new_dict.get(item)} '
+
+print(new_string)
+
+# Задача 22: Даны два неупорядоченных набора целых чисел (может быть, с повторениями). Выдать без повторений в порядке
+# возрастания все те числа, которые встречаются в обоих наборах.
+# Пользователь вводит 2 числа. n — кол-во элементов первого множества. m — кол-во элементов второго множества.
+# Затем пользователь вводит сами элементы множеств.
+
+first_list = []
+second_list = []
+user_size_first = int(input('Input size for first list: '))
+user_size_second = int(input('Input size for second list: '))
+
+for i in range(user_size_first):
+    number = randint(0, 10)
+    first_list.append(number)
+
+for i in range(user_size_second):
+    number = randint(0, 10)
+    second_list.append(number)
+
+first_set = set(first_list)
+second_set = set(second_list)
+new_list = sorted(list(first_set.intersection(second_set)))
+
+print(first_set, second_set)  # Для проверки
+print(new_list)
+
+# Задача 24: В фермерском хозяйстве в Карелии выращивают чернику. Она растёт на круглой грядке, причём кусты высажены
+# только по окружности. Таким образом, у каждого куста есть ровно два соседних. Всего на грядке растёт N кустов.
+# Эти кусты обладают разной урожайностью, поэтому ко времени сбора на них выросло различное число ягод — на i-ом кусте
+# выросло ai ягод. В этом фермерском хозяйстве внедрена система автоматического сбора черники. Эта система состоит
+# из управляющего модуля и нескольких собирающих модулей. Собирающий модуль за один заход, находясь непосредственно
+# перед некоторым кустом, собирает ягоды с этого куста и с двух соседних с ним. Напишите программу для нахождения
+# максимального числа ягод, которое может собрать за один заход собирающий модуль, находясь перед некоторым кустом
+# заданной во входном файле грядки.
+# Пример:
+# 4 -> 1 2 3 4
+# 9
+# Визуализируем: m - модуль сбора
+# 1 2 <-\
+# 3 4 <-|
+# ^     |
+# |-----m
+
+total_bushes = int(input('Input the total number of bushes: '))
+shrub_number = int(input('Input the sequence number of the shrub: '))  # 1 <= shrub_number <= total_bushes
+total_berries = []
+modul_pick = 0
+
+for item in range(total_bushes):
+    berries = randint(1, 10)
+    total_berries.append(berries)
+
+print(total_berries)  # Для проверки
+
+if shrub_number == total_bushes:
+    """Если брать логику архитектуры примера, то последний элемент соседствует с двумя предыдущими кустами"""
+    modul_pick = total_berries[shrub_number - 3] + total_berries[shrub_number - 2] + total_berries[shrub_number - 1]
+    print(modul_pick)
+elif shrub_number == 1:
+    """Аналогично взял логику архитектуры примера: первый куст соседствует с двумя последующими кустами"""
+    modul_pick = total_berries[shrub_number - 1] + total_berries[shrub_number] + total_berries[shrub_number + 1]
+    print(modul_pick)
+elif 1 < shrub_number < total_bushes:
+    """Если брать логику реальной жизни, то соседними будут куст перед выбранным и куст после выбранного"""
+    modul_pick = total_berries[shrub_number - 2] + total_berries[shrub_number - 1] + total_berries[shrub_number]
+    print(modul_pick)
+else:
+    """Невозможно вбить порядковое значение куста больше, чем есть всего кустов. 
+    Также невозможно вбить порядковый номер меньше либо равный 0"""
+    print('Incorrect the number of bushes')
