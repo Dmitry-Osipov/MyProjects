497767e84f2e91b4006bccf563382388c771ba9f
Dmitry-Osipov <dimaosipov00@gmail.com>
2023-09-15 17:37:50 +0300

    Double-linked list. Structure and basic operations.

--- Python_Knowleges/Stepik/Data_Structures/double_linked_list.py
+++ Python_Knowleges/Stepik/Data_Structures/double_linked_list.py
@@ -81,4 +81,133 @@ node (node.prev = None), после чего переносим ссылку hea
 |=========================================================|
 """
 
-# TODO: написать реализацию двусвязного списка кодом. Код взять из прошлых уроков.
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.prev = None
+        self.next = None
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def push_back(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = self.tail = new_node
+        else:
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
+
+    def push_front(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+
+    def pop_back(self):
+        if not self.tail:
+            raise Exception("List is empty")
+        data = self.tail.data
+        if self.head == self.tail:
+            self.head = self.tail = None
+        else:
+            self.tail = self.tail.prev
+            self.tail.next = None
+        return data
+
+    def pop_front(self):
+        if not self.head:
+            raise Exception("List is empty")
+        data = self.head.data
+        if self.head == self.tail:
+            self.head = self.tail = None
+        else:
+            self.head = self.head.next
+            self.head.prev = None
+        return data
+
+    def insert(self, prev_node_data, data):
+        new_node = Node(data)
+        current = self.head
+        while current:
+            if current.data == prev_node_data:
+                new_node.next = current.next
+                new_node.prev = current
+                if current.next:
+                    current.next.prev = new_node
+                current.next = new_node
+                if current == self.tail:
+                    self.tail = new_node
+                return
+            current = current.next
+        raise ValueError(f"Node with data {prev_node_data} not found")
+
+    def erase(self, data):
+        current = self.head
+        while current:
+            if current.data == data:
+                if current.prev:
+                    current.prev.next = current.next
+                else:
+                    self.head = current.next
+                if current.next:
+                    current.next.prev = current.prev
+                else:
+                    self.tail = current.prev
+                return
+            current = current.next
+        raise ValueError(f"Node with data {data} not found")
+
+    def at(self, data):
+        current = self.head
+        while current:
+            if current.data == data:
+                return True
+            current = current.next
+        return False
+
+    def print_list(self):
+        current = self.head
+        while current:
+            print(current.data, end=" ")
+            current = current.next
+        print()
+
+
+# Создаем объект двусвязного списка
+dll = DoublyLinkedList()
+
+# Добавление элементов в конец
+dll.push_back(10)
+dll.push_back(20)
+dll.push_back(30)
+
+# Добавление элементов в начало
+dll.push_front(5)
+dll.push_front(2)
+
+# Удаление элемента с конца
+print("Pop back:", dll.pop_back())  # Output: Pop back: 30
+
+# Удаление элемента с начала
+print("Pop front:", dll.pop_front())  # Output: Pop front: 2
+
+# Вставка элемента после указанного
+dll.insert(10, 15)
+
+# Удаление элемента по значению
+dll.erase(20)
+
+# Проверка наличия элемента
+print("Contains 20:", dll.at(20))  # Output: Contains 20: False
+
+# Вывод списка
+dll.print_list()  # Output: 5 10 15
